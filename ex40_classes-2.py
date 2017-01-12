97bb4565d51eecc8cac20ac1be29fe9bd808bdbe
avico83 <avi.cohen83@gmail.com>
2017-01-12 09:54:38 +0200

    32

--- ex40_classes-2.py
+++ ex40_classes-2.py
@@ -3,6 +3,8 @@ class song(object):
     def __init__(self, lyrics):
         self.lyrics = lyrics
 
+
+
     def sing_me_a_song(self):
         for line in self.lyrics:
             print line
@@ -10,6 +12,9 @@ class song(object):
 happy_bday = song(["Happy birthday to you",
                    "I don't want to get sued",
                    "So i'll stop right here"])
+
+bulls_on_parade = song(["They rally around tha family",
+                        "With pockets full of shells"])
 
 
 
--- my_classes.py
+++ my_classes.py
@@ -1,10 +1,10 @@
 class my_song(object):
 
-    def __init__(self, lyrics):
+    def __init__(self,lyrics):
         self.lyrics = lyrics
 
     def sing_my_song(self):
-        for words in lyrics:
+        for words in self.lyrics:
             print words
 
 bulls_on_parade = my_song(["They rally around tha family",
